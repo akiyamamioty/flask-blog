da8838f3c71f5fcabe5fb658001912bbfb8a7a2e
akiyamamioty <672795574@qq.com>
2016-11-19 23:09:18 +0800

    todoindex

--- run.py
+++ run.py
@@ -1,1 +1,15 @@
 # coding=utf-8
+
+import os 
+from flask import Flask
+from flask_script import Manager
+
+app = Flask(__name__)
+manager = Manager(app)
+
+@app.route('/')
+def index():
+    pass
+
+if __name__ == '__main__':
+    manager.run()

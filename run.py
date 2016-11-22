1eeb43b1984780e5b581a26d405e33b767e64db1
akiyamamioty <672795574@qq.com>
2016-11-22 23:08:58 +0800

    可以显示文章，数据库迁移未做，html删除2行要及时补

--- run.py
+++ run.py
@@ -8,6 +8,7 @@ from flask import g
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Shell
 import hashlib
+from flask_migrate import Migrate, MigrateCommand
 import sys
 reload(sys)
 sys.setdefaultencoding( "utf-8" )
@@ -15,18 +16,20 @@ sys.setdefaultencoding( "utf-8" )
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 manager = Manager(app)
 app.config['SQLALCHEMY_DATABASE_URI'] =\
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 SECRET_KEY = os.environ.get('SECRET_KEY') or 'foolish'
-db = SQLAlchemy(app)
 app.config.from_object(__name__)
 
 #集成python shell 调用shell时候将数据库对象直接导入shell
 def make_shell_context():
     return dict(app=app, db=db, Post=Post)
 manager.add_command("shell", Shell(make_context=make_shell_context))
+manager.add_command("db", MigrateCommand)
 
 #404和500界面
 @app.errorhandler(404)
@@ -40,6 +43,8 @@ def internal_server_error(e):
 @app.route('/')
 def index():
     tem = Post.query.all()  
+    print type(tem)
+    print tem[0].title
     pmax = 9
     if False:
         session['new']=True
@@ -60,8 +65,39 @@ def login():
         m.update(pass_word)
         if m.hexdigest()=='01bf2bf3375b3fbaf8d2dac6dad08c84':
             session['log'] = True
+            print session['log']
             return redirect(url_for('admin'))
     return render_template('login.html')
+
+#创建新博文 todo
+@app.route('/new', methods=['GET', 'POST'])
+def new():
+    if session.get('log'):
+        if request.method == 'POST':
+            if request.form['editor'] and request.form['title']:
+                abstract = abstr(request.form['editor'],request.form['img'])
+                tags = (request.form['tags'] or '').replace('，',',')
+                post = Post(title=request.form['title'], content=request.form['editor'], \
+                    abstract=abstract, tags=tags,)
+                db.session.add(post)
+                try:
+                    db.session.commit()
+                except:
+                    db.session.rollback()
+                '''
+                cur.execute('select id from blog order by id desc limit 1')
+                blog=cur.fetchall()
+                blog=blog[0][0]
+                tags=tags.split(',')
+                for tag in tags:
+                    cur.execute('insert into tag (tag, blog) values (?, ?)', (tag, blog))
+                get_db().commit()
+                '''
+                return redirect(url_for('index',pg=1))
+            elif request.form['editor']:
+                return render_template('edit.html',content=request.form['editor'],img=request.form['img'])
+        return render_template('edit.html')
+    return redirect(url_for('page',pg=1))
 
 
 #数据库模型
@@ -69,8 +105,33 @@ class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
-    body = db.Column(db.Text)
-    timestamp = db.Column(db.DateTime, index=True)
+    content = db.Column(db.Text)
+    abstract = db.Column(db.Text)
+    tags = db.Column(db.String)
+    #timestamp = db.Column(db.DateTime, index=True)
+
+
+
+def abstr(text,img=""):
+    text=text[:1200]
+    text=text.replace(u'&nbsp;',u' ')
+    text=text.replace(u'</p',u'\n<')
+    text=text.replace(u'</b',u'\n<')
+    text=text.replace(u'</h',u'\n<')
+    text=text.replace(u'<br>',u'\n')
+    def fn(x, y):
+        if x[-1] == "<" and y != ">":
+            return x
+        else:
+            return x+y
+    text=reduce(fn,text)
+    text=text.replace(u'<>',u'')
+    text=text.replace(u'\n\n\n',u'\n')
+    text=text.replace(u'\n\n',u'\n')
+    print text
+    text=text[:120]+'...'+'<center>'+img+'</center>'
+    return text
+
 
 if __name__ == '__main__':
     manager.run()
